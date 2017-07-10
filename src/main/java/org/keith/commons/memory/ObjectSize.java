723db1360e53057d0adf7914bbd375f93882b495
wubinbin <wubinbin@geekplus.com.cn>
2017-07-10 16:58:07 +0800

    11

---
+++ src/main/java/org/keith/commons/memory/ObjectSize.java
@@ -0,0 +1,23 @@
+package org.keith.commons.memory;
+
+import java.lang.instrument.Instrumentation;
+
+public class ObjectSize {
+	private static volatile Instrumentation instru;
+	 
+    public static void premain(String args, Instrumentation inst) {
+        instru = inst;
+    }
+ 
+    /**
+     * get object byte size
+     * @param object
+     * @return
+     */
+    public static Long getSizeOf(Object object) {
+        if (instru == null) {
+            throw new IllegalStateException("Instrumentation is null");
+        }
+        return instru.getObjectSize(object);
+    }
+}
