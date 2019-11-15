b011b8d36d1e990f7e414f621989ca18f9e89ec2
wubb <wubb@zetyun.com>
2019-11-15 12:18:23 +0800

    1

--- src/main/java/org/keith/commons/pdf/Test.java
+++ src/main/java/org/keith/commons/pdf/Test.java
@@ -3,7 +3,9 @@ package org.keith.commons.pdf;
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
+import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.WriterProperties;
 import com.itextpdf.layout.font.FontProvider;
 
 import java.io.File;
@@ -20,10 +22,9 @@ public class Test {
         file.mkdirs();
 
         StringBuilder HTML = new StringBuilder();
-        HTML.append("<h4 style='margin:5px;'>模型评估：image-classification-GoogleNet</h4>");
-        HTML.append("<img style='display:block;' src=\"img/1.png\">");//style='display:block;margin-bottom:5px;'
-        HTML.append("<img style='display:block;' src=\"img/1.png\">");
-        HTML.append("<img style='display:block;' src=\"img/1.png\">");
+        HTML.append("<h3 style='margin:0px;'>模型评估：image-classification-GoogleNet</h3>");
+        HTML.append("<h4 style='margin:0px;'>评估子标题</h4>");
+        HTML.append("<img style='display:block;width:100%;' src=\"img/1.png\">");//style='display:block;margin-bottom:5px;'
 
         new Test().createPdf(BASEURI, HTML.toString(), DEST);
         System.out.println("ok");
@@ -43,7 +44,7 @@ public class Test {
         properties.setBaseUri(baseUri);
         properties.setFontProvider(fontProvider);
         properties.setCharset("UTF-8");
-        File pdfFile = new File(dest);
-        HtmlConverter.convertToPdf(html, new PdfWriter(pdfFile), properties);
+        PdfWriter pdfWriter = new PdfWriter(dest, new WriterProperties().setFullCompressionMode(false));
+        HtmlConverter.convertToPdf(html, pdfWriter, properties);
     }
 }
